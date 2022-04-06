72039d4836af481b84e0de72f3323b4ecd058776
mesmerz <mileikamarks@gmail.com>
2022-04-06 20:39:54 +0300

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template >
+  <div class="article">
+      <section v-for="(article, index) in articles" :key="index">
+        <h2>{{ article.headline }}</h2>
+        <img class="image" :src="article.media[0].sources.landscape.large">
+
+      </section>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      articles: []
+    }
+  },
+  async fetch() {
+    this.articles = await fetch(
+      'https://services.postimees.ee/rest/v1/sections/81/editorsChoice/articles?limit=5'
+    ).then(res => res.json())
+  }
+}
+</script>
+
+<style>
+.image {
+  text-align: right;
+  width: 780px;
+}
+.article {
+  margin-left: 100px;
+}
+</style>
